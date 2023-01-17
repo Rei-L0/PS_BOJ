d50eda82ee799c5745b173b4e6df2db3dbb044f1
Rei <86551168+Rei-L0@users.noreply.github.com>
2023-01-17 16:14:34 +0900

    [Silver I] Title: 오르막 수, Time: 56 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+dp = [[0 for _ in range(10)]for _ in range(1001)]
+
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2, 1001):
+    for j in range(10):
+        for k in range(10):
+            if j >= k:
+                dp[i][j] += dp[i-1][k] % 10007
+            else:
+                break
+
+print(sum(dp[N]) % 10007)
