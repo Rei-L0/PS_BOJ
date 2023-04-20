8b753dedf6e3b78d86d416acd38817a22716dffc
Rei <86551168+Rei-L0@users.noreply.github.com>
2023-04-20 22:44:42 +0900

    [level 2] Title: 튜플, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,13 +1,9 @@
+from collections import Counter
+
 def solution(s):
     answer = []
     s=list(map(int,s.replace('{','').replace('}','').split(',')))
-    s_dict={}
-    for i in s:
-        if i not in s_dict:
-            s_dict[i]=1
-        else:
-            s_dict[i]+=1
-    s_dict=sorted(s_dict.items(),key=lambda x: x[1],reverse=True)
-    for k,v in s_dict:
-        answer.append(k)
+    s_dict=Counter(s).most_common()
+    for i in s_dict:
+        answer.append(i[0])
     return answer
