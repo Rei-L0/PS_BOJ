e54af8a59a4ac569c3f0a01839f4228faf6719bd
Rei <qkrdusgn00@naver.com>
2023-05-04 18:59:34 +0900

    [Silver III] Title: N과 M (3), Time: 1368 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+s = []
+
+
+def NM():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, N+1):
+        s.append(i)
+        NM()
+        s.pop()
+
+
+NM()
