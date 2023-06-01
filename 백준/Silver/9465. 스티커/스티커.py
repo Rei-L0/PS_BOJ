726af12a703da1c29c247b00c3d68bcdd0be69a1
Rei <qkrdusgn00@naver.com>
2023-06-01 18:50:37 +0900

    [Silver I] Title: 스티커, Time: 832 ms, Memory: 43084 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    sticker = list()
+    dp = [[0 for _ in range(n)] for _ in range(2)]
+    for i in range(2):
+        x = list(map(int, input().split()))
+        sticker.append(x)
+    dp[0][0], dp[1][0] = sticker[0][0], sticker[1][0]
+    if n > 1:
+        dp[0][1] = dp[1][0] + sticker[0][1]
+        dp[1][1] = dp[0][0] + sticker[1][1]
+        for i in range(2, n):
+            dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + sticker[0][i]
+            dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + sticker[1][i]
+    print(max(max(dp[0]), max(dp[1])))
