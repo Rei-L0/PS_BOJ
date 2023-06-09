3f98207d1e24a3bc73ec1f23792a83d444787655
Rei <qkrdusgn00@naver.com>
2023-06-09 15:07:29 +0900

    [Silver II] Title: 이동하기, Time: 868 ms, Memory: 70824 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+dp = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        if i == 0 and j == 0:
+            continue
+        elif i == 0 and j > 0:
+            dp[i][j] += dp[i][j - 1]
+        elif j == 0 and i > 0:
+            dp[i][j] += dp[i - 1][j]
+        else:
+            dp[i][j] += max(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+
+print(dp[n - 1][m - 1])
