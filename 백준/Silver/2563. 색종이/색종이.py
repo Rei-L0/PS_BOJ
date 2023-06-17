0ef6fe15bb9009c51c61eaa9e2766f97412b7fa8
Rei <qkrdusgn00@naver.com>
2023-06-17 15:58:07 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,14 @@
+color = [[0] * 101 for _ in range(101)]
+
+n = int(input())
+
+for _ in range(n):
+    x, y = map(int, input().split())
+
+    for i in range(y, y + 10):
+        for j in range(x, x + 10):
+            color[i][j] = 1
+
+ans = [sum(color[i]) for i in range(1, 101)]
+
+print(sum(ans))
