cc8d3abd435ba3d56a3c3c9d41762ee01a64c1ba
Rei <qkrdusgn00@naver.com>
2023-07-05 14:51:27 +0900

    [Gold V] Title: 토마토, Time: 2236 ms, Memory: 98596 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,7 +4,6 @@ from collections import deque
 n, m = map(int, input().split())
 tomato = [list(map(int, input().split())) for _ in range(m)]
 queue = deque()
-visit = [[False] * n for _ in range(m)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
@@ -18,12 +17,11 @@ ans = 0
 flag = 1
 while queue:
     x, y = queue.popleft()
-    visit[y][x] = True
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if -1 < nx < n and -1 < ny < m:
-            if not visit[ny][nx] and tomato[ny][nx] == 0:
+            if tomato[ny][nx] == 0:
                 queue.append((nx, ny))
                 tomato[ny][nx] = tomato[y][x] + 1
 for i in tomato:
