460d4fe606e9c654e2ca6376c52c180c178dfb07
Rei <qkrdusgn00@naver.com>
2023-07-06 12:19:52 +0900

    [Gold V] Title: 내려가기, Time: 324 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+max_val = [0, 0, 0]
+min_val = [10, 10, 10]
+
+for i in range(n):
+    l, c, r = map(int, sys.stdin.readline().rstrip().split())
+    if i == 0:
+        max_val = [l, c, r]
+        min_val = [l, c, r]
+    else:
+        max_val = [
+            l + max(max_val[0], max_val[1]),
+            c + max(max_val[0], max_val[1], max_val[2]),
+            r + max(max_val[1], max_val[2]),
+        ]
+        min_val = [
+            l + min(min_val[0], min_val[1]),
+            c + min(min_val[0], min_val[1], min_val[2]),
+            r + min(min_val[1], min_val[2]),
+        ]
+print(f"{max(max_val)} {min(min_val)}")
