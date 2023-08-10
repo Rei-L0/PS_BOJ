9aa02012600b10b5eb427e447bc43a762ab80eeb
Rei <qkrdusgn00@naver.com>
2023-08-10 17:14:11 +0900

    [Gold V] Title: 달력, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/20207. 달력/달력.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0] * 366
+ans = 0
+
+for i in range(n):
+    start, end = map(int, input().split())
+    while start <= end:
+        arr[start] += 1
+        start += 1
+
+rows, cols = 0, 0
+
+for i in range(366):
+    if arr[i] == 0:
+        ans += rows * cols
+        rows, cols = 0, 0
+        continue
+
+    rows += 1
+    cols = max(cols, arr[i])
+
+print(ans + rows * cols)
