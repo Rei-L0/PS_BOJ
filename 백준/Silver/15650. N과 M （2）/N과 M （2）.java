e07650851639b462b7cd05a2cb6b480d120c379a
Rei <qkrdusgn00@naver.com>
2024-01-31 12:03:26 +0900

    [Silver III] Title: N과 M (2), Time: 152 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,29 +1,49 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(bReader.readLine());
-		n = Integer.parseInt(stringTokenizer.nextToken());
-		m = Integer.parseInt(stringTokenizer.nextToken());
-		combi(new int[m], 0, 1);
+	static void print() {
+		for(int i=0;i<M;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
 	}
-
-	// 조합
-	private static void combi(int[] numbers, int cnt, int start) {
-		if (cnt == m) {
-			for (int i : numbers)
-				System.out.print(i + " ");
-			System.out.println();
+	
+	static void combi(int cnt,int start) {
+		if(cnt == M) {
+			print();
 			return;
 		}
-		for (int i = start; i <= n; i++) {
-			numbers[cnt] = i;
-			combi(numbers, cnt + 1, i + 1);
+		for(int i=start;i<=N;i++) {
+			if(visited[i]) continue;
+			visited[i] = true;
+			arr[cnt] = i;
+			combi(cnt+1,i+1);
+			visited[i] =false;
 		}
+		
 	}
+	static boolean[] visited = new boolean[10];
+	static int[] arr = new int[10];
+	static int N,M;
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st= new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		combi(0,1);
+		
+		
+		
+		
+	}
+
 }
