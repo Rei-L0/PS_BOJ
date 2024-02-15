762aa959315c753bd4e5933399e4dd49e1e7118f
Rei <qkrdusgn00@naver.com>
2024-02-15 23:25:29 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 128 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Silver IV/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[][] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
+
+        int idx = 0;
+        int time = 0;
+        while (idx != n) {
+            if (time < arr[idx][0]) {
+                time = arr[idx][0];
+            }
+            time += arr[idx][1];
+            idx++;
+        }
+
+        System.out.println(time);
+
+    }
+}
