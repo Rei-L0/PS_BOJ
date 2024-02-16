24a0e44a3bef47f391f1c99f1dcf8294fca34d04
Rei <qkrdusgn00@naver.com>
2024-02-16 11:05:05 +0900

    [Gold II] Title: 면접보는 승범이네, Time: 1124 ms, Memory: 177764 KB -BaekjoonHub

--- 백준/Gold II/17835. 면접보는 승범이네/면접보는 승범이네.java
+++ 백준/Gold II/17835. 면접보는 승범이네/면접보는 승범이네.java
@@ -1,75 +1,85 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
-        List<List<Edge>> graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<Edge>());
-        }
-        
-        for (int i = 0; i < M; i++ ) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int dist = Integer.parseInt(st.nextToken());
-            graph.get(v).add(new Edge(u, dist));
-        }
-        
-        long ans = 0;
-        int ansLoc = 0;
-        st = new StringTokenizer(br.readLine());
-        long[] dist = new long[N+1];
-        Arrays.fill(dist, 100_000_000_000L);
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        for (int i = 0; i < K; i++) {
-            int exam = Integer.parseInt(st.nextToken());
-            pq.offer(new Edge(exam, 0));
-            dist[exam] = 0;
-        }
-        
-        while (!pq.isEmpty()) {
-            Edge cur = pq.poll();
-            int curLoc = cur.loc;
-            long curDist = cur.dist;
-            if (dist[curLoc] < curDist) continue;
-            for (Edge nxt: graph.get(curLoc)) {
-                int nxtLoc = nxt.loc;
-                long nxtDist = curDist + nxt.dist;
-                if (nxtDist < dist[nxtLoc]) {
-                    dist[nxtLoc] = nxtDist;
-                    pq.offer(new Edge(nxtLoc, nxtDist));
-                }
-            }
-        }
-        
-        for (int i = 1; i <= N; i++) {
-            if (ans < dist[i]) {
-                ans = dist[i];
-                ansLoc = i;
-            }
-        }
-        System.out.println(ansLoc);
-        System.out.println(ans);
-    }
-    
-    static class Edge implements Comparable<Edge>{
-        int loc;
-        long dist;
-        
-        public Edge(int loc, long dist) {
-            this.loc = loc;
-            this.dist = dist;
-        }
- 
-        public int compareTo(Edge o) {
-            return Long.compare(this.dist, o.dist);
-        }
-    }
+
+	static int n, m, k;
+	static long maxValue, maxNode;
+	static ArrayList<ArrayList<Node>> arrayList;
+	static long[] distance;
+	static PriorityQueue<Node> pQueue = new PriorityQueue<>();
+
+	static class Node implements Comparable<Node> {
+		int to;
+		long c;
+
+		public Node(int to, long c) {
+			this.to = to;
+			this.c = c;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Long.compare(this.c, o.c);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bReader.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		arrayList = new ArrayList<>();
+		for (int i = 0; i < n + 1; i++)
+			arrayList.add(new ArrayList<Node>());
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(bReader.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			arrayList.get(v).add(new Node(u, c));
+		}
+
+		distance = new long[n + 1];
+		Arrays.fill(distance, 100_000_000_000L);
+
+		st = new StringTokenizer(bReader.readLine());
+		for (int i = 0; i < k; i++) {
+			int s = Integer.parseInt(st.nextToken());
+			distance[s] = 0;
+			pQueue.add(new Node(s, 0));
+		}
+
+		while (!pQueue.isEmpty()) {
+			Node now = pQueue.poll();
+			if (distance[now.to] < now.c)
+				continue;
+			for (int i = 0; i < arrayList.get(now.to).size(); i++) {
+				Node next = arrayList.get(now.to).get(i);
+				if (distance[next.to] > now.c + next.c) {
+					distance[next.to] = now.c + next.c;
+					pQueue.add(new Node(next.to, distance[next.to]));
+				}
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (maxValue < distance[i]) {
+				maxNode = i;
+				maxValue = distance[i];
+			}
+		}
+
+		System.out.println(maxNode);
+		System.out.println(maxValue);
+
+	}
 }
