0bef0265f1745f6aacad78c4355373ac24ef0e50
Rei <qkrdusgn00@naver.com>
2024-02-19 21:31:54 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 708 ms, Memory: 68312 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -0,0 +1,91 @@
+// 1504 특정한 최단 경로
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, e, v1, v2;
+    static long ans = 0;
+    static ArrayList<ArrayList<Node>> arrayList;
+
+    static class Node implements Comparable<Node> {
+
+        int to;
+        long c;
+
+        public Node(int to, long c) {
+            this.to = to;
+            this.c = c;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Long.compare(this.c, o.c);
+        }
+    }
+
+    static long solve() {
+        ans = Math.min(dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n),
+            dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n));
+        return ans >= Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    static long dijkstra(int start, int end) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        long[] distance = new long[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            if (distance[now.to] < now.c) {
+                continue;
+            }
+            for (int i = 0; i < arrayList.get(now.to).size(); i++) {
+                Node next = arrayList.get(now.to).get(i);
+                if (distance[next.to] > now.c + next.c) {
+                    distance[next.to] = now.c + next.c;
+                    pq.add(new Node(next.to, distance[next.to]));
+                }
+            }
+        }
+        return distance[end];
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bReader.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+
+        arrayList = new ArrayList<>();
+
+        for (int i = 0; i < n + 1; i++) {
+            arrayList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(bReader.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            arrayList.get(s).add(new Node(end, w));
+            arrayList.get(end).add(new Node(s, w));
+        }
+
+        st = new StringTokenizer(bReader.readLine());
+        v1 = Integer.parseInt(st.nextToken());
+        v2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(solve());
+
+    }
+}
