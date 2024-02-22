4a00b60bf3b2bb879c72fbe150c33bd5bdbee233
Rei <qkrdusgn00@naver.com>
2024-02-22 11:42:32 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,714 ms, Memory: 118,240 KB -BaekjoonHub

---
+++ SWEA/D4/3124. 최소 스패닝 트리/README.md
@@ -0,0 +1,15 @@
+# [D4] 최소 스패닝 트리 - 3124 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV_mSnmKUckDFAWb) 
+
+### 성능 요약
+
+메모리: 118,240 KB, 시간: 1,714 ms, 코드길이: 1,865 Bytes
+
+### 제출 일자
+
+2024-02-22 11:41
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int v, e, t;
+	static long ans;
+
+	static ArrayList<Node> graph;
+	static int[] p;
+
+	static BufferedReader bReader;
+	static StringTokenizer st;
+
+	static class Node implements Comparable<Node> {
+		int s;
+		int t;
+		int w;
+
+		public Node(int s, int t, int w) {
+			this.s = s;
+			this.t = t;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.w, o.w);
+		}
+
+	}
+
+	static void init() throws Exception {
+		st = new StringTokenizer(bReader.readLine());
+		v = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+
+		graph = new ArrayList<>();
+		p = new int[v + 1];
+		for (int i = 0; i < v + 1; i++) {
+			p[i] = i;
+		}
+		ans = 0;
+		for (int i = 0; i < e; i++) {
+			st = new StringTokenizer(bReader.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			graph.add(new Node(s, t, w));
+		}
+
+		Collections.sort(graph);
+
+	}
+
+	static int find(int x) {
+		if (x == p[x])
+			return x;
+		return p[x] = find(p[x]);
+	}
+
+	static boolean union(int x, int y) {
+		x = find(x);
+		y = find(y);
+
+		if (x == y)
+			return false;
+
+		p[x] = p[y];
+		return true;
+	}
+
+	static void solve() {
+		for (int i = 0; i < graph.size(); i++) {
+			Node now = graph.get(i);
+			if (union(now.s, now.t))
+				ans += now.w;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		bReader = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(bReader.readLine().trim());
+
+		t = Integer.parseInt(st.nextToken());
+		for (int tc = 1; tc <= t; tc++) {
+			init();
+			solve();
+			System.out.println("#" + tc + " " + ans);
+		}
+
+	}
+}
