7da7e26cc366447682f0674406f5d334dd4cd545
Rei <qkrdusgn00@naver.com>
2024-03-04 23:08:31 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.println("|\\_/|\n"
+            + "|q p|   /}\n"
+            + "( 0 )\"\"\"\\\n"
+            + "|\"^\"`    |\n"
+            + "||_/=\\\\__|");
+    }
+}
