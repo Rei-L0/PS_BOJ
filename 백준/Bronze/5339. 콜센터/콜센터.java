ba2b0cfa8d91e96d3d0b384bc182ae72f3c2a034
Rei <qkrdusgn00@naver.com>
2024-03-04 23:05:14 +0900

    [Bronze V] Title: 콜센터, Time: 120 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.println("     /~\\\n"
+            + "    ( oo|\n"
+            + "    _\\=/_\n"
+            + "   /  _  \\\n"
+            + "  //|/.\\|\\\\\n"
+            + " ||  \\ /  ||\n"
+            + "============\n"
+            + "|          |\n"
+            + "|          |\n"
+            + "|          |");
+    }
+}
