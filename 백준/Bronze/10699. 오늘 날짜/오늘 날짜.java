6ad4e3af1419f946dc665ddeacb377b1b6f88667
Rei <qkrdusgn00@naver.com>
2024-03-04 23:09:00 +0900

    [Bronze V] Title: 오늘 날짜, Time: 120 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.println("2024-03-04");
+    }
+}
