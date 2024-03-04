a2496e2a50e3c6fef85833ab794758af063cb6ad
Rei <qkrdusgn00@naver.com>
2024-03-04 23:09:17 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.println("강한친구 대한육군\n"
+            + "강한친구 대한육군");
+    }
+}
