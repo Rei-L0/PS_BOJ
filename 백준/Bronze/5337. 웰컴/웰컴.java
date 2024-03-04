122d0a4c5fda0cbd8868aedaab77d9f0faf66389
Rei <qkrdusgn00@naver.com>
2024-03-04 23:04:05 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.println(".  .   .\n"
+            + "|  | _ | _. _ ._ _  _\n"
+            + "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
