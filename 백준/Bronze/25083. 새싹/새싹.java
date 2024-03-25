cc2247135c5b9a3dbfd42cca7c4def1ccd849222
Rei <qkrdusgn00@naver.com>
2024-03-25 22:05:36 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("         ,r'\"7\n"
+            + "r`-_   ,'  ,/\n"
+            + " \\. \". L_r'\n"
+            + "   `~\\/\n"
+            + "      |\n"
+            + "      |");
+
+    }
+}
