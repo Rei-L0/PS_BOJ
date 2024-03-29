bb617544c72864a4e0d4b9762e388f80366d6c76
Rei <qkrdusgn00@naver.com>
2024-03-29 16:19:10 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 588 ms, Memory: 100428 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -71,6 +71,7 @@ public class Main {
 
 		while (!q.isEmpty()) {
 			Pos now = q.poll();
+			// 현재 좌표의 다음 방향으로 갈 수 있는 좌표 구하기
 			int d = dir.get(board[now.x][now.y]);
 			int nx = now.x + dx[d];
 			int ny = now.y + dy[d];
@@ -78,58 +79,33 @@ public class Main {
 				q.add(new Pos(nx, ny));
 				check[nx][ny] = true;
 			}
+			// 다른 방향에서 현재 좌표로 올 수 있는 경우 구하기
 			for (int i = 0; i < 4; i++) {
 				if (i == d)
 					continue;
-				if (i == 0) {
-					nx = now.x + dx[i];
-					ny = now.y + dy[i];
-					if (nx < 0 || ny < 0 || nx >= r || ny >= c)
-						continue;
-					if (check[nx][ny])
-						continue;
-					if (board[nx][ny] == 'D') {
-						q.add(new Pos(nx, ny));
-						check[nx][ny] = true;
-					}
+				nx = now.x + dx[i];
+				ny = now.y + dy[i];
+				if (nx < 0 || ny < 0 || nx >= r || ny >= c)
+					continue;
+				if (check[nx][ny])
+					continue;
+				if (board[nx][ny] == reverse(i)) {
+					q.add(new Pos(nx, ny));
+					check[nx][ny] = true;
 				}
-				if (i == 1) {
-					nx = now.x + dx[i];
-					ny = now.y + dy[i];
-					if (nx < 0 || ny < 0 || nx >= r || ny >= c)
-						continue;
-					if (check[nx][ny])
-						continue;
-					if (board[nx][ny] == 'U') {
-						q.add(new Pos(nx, ny));
-						check[nx][ny] = true;
-					}
-				}
-				if (i == 2) {
-					nx = now.x + dx[i];
-					ny = now.y + dy[i];
-					if (nx < 0 || ny < 0 || nx >= r || ny >= c)
-						continue;
-					if (check[nx][ny])
-						continue;
-					if (board[nx][ny] == 'R') {
-						q.add(new Pos(nx, ny));
-						check[nx][ny] = true;
-					}
-				}
-				if (i == 3) {
-					nx = now.x + dx[i];
-					ny = now.y + dy[i];
-					if (nx < 0 || ny < 0 || nx >= r || ny >= c)
-						continue;
-					if (check[nx][ny])
-						continue;
-					if (board[nx][ny] == 'L') {
-						q.add(new Pos(nx, ny));
-						check[nx][ny] = true;
-					}
-				}
+
 			}
 		}
+	}
+
+	static char reverse(int x) {
+		if (x == 0)
+			return 'D';
+		else if (x == 1)
+			return 'U';
+		else if (x == 2)
+			return 'R';
+		else
+			return 'L';
 	}
 }
