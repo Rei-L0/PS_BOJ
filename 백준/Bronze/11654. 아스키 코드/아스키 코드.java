10749d7f6291daaf388b0b7b0a4e312173486199
Rei <qkrdusgn00@naver.com>
2024-04-04 22:27:30 +0900

    [Bronze V] Title: 아스키 코드, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println((int) br.readLine().charAt(0));
+    }
+}
