9fb73b95cef47d0cd9db0823bc82dad67c73448d
Rei <qkrdusgn00@naver.com>
2024-04-04 22:32:02 +0900

    [Bronze V] Title: 身長 (Height), Time: 80 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(b - a);
+    }
+}
