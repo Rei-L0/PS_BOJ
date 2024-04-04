aaabaf732bd485f6e9ec755fa83e019531a36853
Rei <qkrdusgn00@naver.com>
2024-04-04 22:36:19 +0900

    [Bronze V] Title: A+B - 3, Time: 84 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(n + m).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
