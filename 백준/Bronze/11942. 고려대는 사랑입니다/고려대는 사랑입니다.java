f675d4aab5e7453cad016daba1181e2d03e180ce
Rei <qkrdusgn00@naver.com>
2024-04-07 18:44:46 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 72 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("고려대학교");
+    }
+}
