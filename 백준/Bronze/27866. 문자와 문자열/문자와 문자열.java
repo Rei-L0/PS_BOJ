b2b03d36ca2b2fd32c29788e5053db59c52da48a
Rei <qkrdusgn00@naver.com>
2024-04-10 00:53:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.println(s.charAt(i - 1));
+    }
+}
