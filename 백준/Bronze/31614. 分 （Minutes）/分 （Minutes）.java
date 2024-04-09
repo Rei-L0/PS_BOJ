72479d169548eef362bcf1cb35ff045fa078ae04
Rei <qkrdusgn00@naver.com>
2024-04-10 00:45:51 +0900

    [Bronze V] Title: 分 (Minutes), Time: 80 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        int s = Integer.parseInt(br.readLine());
+
+        System.out.println(m * 60 + s);
+    }
+}
