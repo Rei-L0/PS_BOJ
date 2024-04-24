b38fdc990f04dcdfc093b28da744595a66d7724d
Rei <qkrdusgn00@naver.com>
2024-04-24 23:58:57 +0900

    [Bronze V] Title: 구구단, Time: 76 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int n;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+    }
+
+}
