02eca7f1261363792411ddc8f1a0d21b79a0c31e
Rei <qkrdusgn00@naver.com>
2024-05-10 23:56:01 +0900

    [Bronze IV] Title: 영수증, Time: 84 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sum += (a * b);
+        }
+
+        System.out.println((sum == x) ? "Yes" : "No");
+    }
+
+}
