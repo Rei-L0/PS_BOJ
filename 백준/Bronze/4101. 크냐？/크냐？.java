542192994f2c6ce8849f50e9bc4677400d45ff9e
Rei <qkrdusgn00@naver.com>
2024-05-15 11:32:06 +0900

    [Bronze V] Title: 크냐?, Time: 80 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            sb.append((a > b) ? "Yes" : "No").append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
