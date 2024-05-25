939350905cc369aa9785e8f4cc0a1671a5889bf2
Rei <qkrdusgn00@naver.com>
2024-05-25 13:25:23 +0900

    [Bronze IV] Title: 기찍 N, Time: 164 ms, Memory: 17148 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i > 0; i--) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+}
