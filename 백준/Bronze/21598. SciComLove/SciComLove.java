996925b705182526756c457b536c9de2e1af3370
Rei <qkrdusgn00@naver.com>
2024-06-11 11:52:53 +0900

    [Bronze V] Title: SciComLove, Time: 72 ms, Memory: 11292 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("SciComLove");
+        }
+
+    }
+
+
+}
