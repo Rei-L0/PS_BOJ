674104626557d0ac27c0861ca0f1836e53e7f40a
Rei <qkrdusgn00@naver.com>
2024-06-21 16:58:43 +0900

    [Silver III] Title: 에너지 드링크, Time: 252 ms, Memory: 27124 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int max = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+
+        double ans = (double) max / 2;
+
+        for (int i = 0; i < n; i++) {
+            ans += (double) arr[i] / 2;
+        }
+
+        System.out.println(ans);
+    }
+}
