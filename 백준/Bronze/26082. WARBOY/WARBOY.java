25e1f9377bbc0990acb7d71186ef045f1c2b4ed5
Rei <qkrdusgn00@naver.com>
2024-07-12 21:46:29 +0900

    [Bronze V] Title: WARBOY, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    static int a, b, c;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        System.out.println((b / a) * 3 * c);
+    }
+
+}
