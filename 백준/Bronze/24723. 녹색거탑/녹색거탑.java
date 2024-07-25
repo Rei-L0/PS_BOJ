1728eeceac97f7638a49389ba2c09e54b186e6c4
Rei <qkrdusgn00@naver.com>
2024-07-25 22:56:19 +0900

    [Bronze IV] Title: 녹색거탑, Time: 76 ms, Memory: 11424 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((int) Math.pow(2, n));
+    }
+}
