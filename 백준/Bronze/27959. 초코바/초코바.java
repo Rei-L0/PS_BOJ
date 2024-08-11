4e792f8f617706e9afb7c3f36d7f5475c3071898
Rei <qkrdusgn00@naver.com>
2024-08-11 12:16:59 +0900

    [Bronze V] Title: 초코바, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println(n * 100 >= m ? "Yes" : "No");
+    }
+
+}
