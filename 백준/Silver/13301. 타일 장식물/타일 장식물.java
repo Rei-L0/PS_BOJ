d0ee1dd6f76f6e665fdc655cbb470f8e4f556abc
Rei <qkrdusgn00@naver.com>
2024-08-19 20:13:28 +0900

    [Silver V] Title: 타일 장식물, Time: 64 ms, Memory: 11496 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n + 1];
+
+        dp[1] = 1;
+        if (n > 1) {
+            dp[2] = 1;
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n] * 4 + dp[n - 1] * 2);
+    }
+
+}
