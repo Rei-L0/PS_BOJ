3679db7d54381b969bd691efba6a833bfe706898
Rei <qkrdusgn00@naver.com>
2024-08-25 01:39:47 +0900

    [Bronze V] Title: 時間 (Hour), Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * 24);
+    }
+
+}
