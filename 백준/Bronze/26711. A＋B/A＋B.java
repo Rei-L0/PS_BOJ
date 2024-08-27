dc485559a0941070cf72f7e1c4e95bf156f2c0e0
Rei <qkrdusgn00@naver.com>
2024-08-27 22:38:16 +0900

    [Bronze V] Title: A+B, Time: 120 ms, Memory: 13268 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+
+        System.out.println(a.add(b));
+    }
+}
