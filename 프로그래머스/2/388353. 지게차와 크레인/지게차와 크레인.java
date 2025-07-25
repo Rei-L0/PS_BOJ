7ed4e352313cd92fc5b91b9c0a41d42643d0289e
Rei <qkrdusgn00@naver.com>
2025-07-25 16:34:58 +0900

    [level 2] Title: 지게차와 크레인, Time: 78.67 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -22,10 +22,9 @@ class Solution {
             }
             else{
                 char front = request.charAt(0);
-                char back = request.charAt(1);
                 for(int i = 0; i < n; i++){
                     for(int j = 0 ; j < m; j++){
-                        if(storage[i].charAt(j) == front || storage[i].charAt(j) == back){
+                        if(storage[i].charAt(j) == front){
                             box[i][j] = true;
                         }
                     }
