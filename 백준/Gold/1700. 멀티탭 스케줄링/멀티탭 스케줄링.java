491338f15e1c3d9f68e1c5979a5015b8778746fa
Rei <qkrdusgn00@naver.com>
2025-09-11 23:24:51 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 68 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -1,70 +1,63 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int n, k, ans;
-
-    static int[] multi, num;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        multi = new int[n];
-
-        num = new int[k];
+    
+    static BufferedReader br;
+    static StringBuilder sb;
+    static StringTokenizer st;
+    
+	public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < k; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[K];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 0; i < k; i++) {
-            if (check(num[i])) {
-                ans++;
-                multi[solve(i)] = num[i];
+        
+        int changeCount = 0;
+        int size = 0;
+        Set<Integer> con = new HashSet<>();
+        for (int i = 0; i < K; i++) {
+            int now = arr[i];
+            
+            if (con.contains(now)) {
+                continue;
             }
-        }
-
-        System.out.println(ans);
-    }
-
-    static boolean check(int x) {
-        for (int i = 0; i < n; i++) {
-            if (multi[i] == 0 || multi[i] == x) {
-                multi[i] = x;
-                return false;
+            
+            if (con.size() < N) {
+                con.add(now);
+                continue;
             }
-        }
-        return true;
-    }
-
-    static int solve(int x) {
-        int res = n;
-        int idx = 0;
-        for (int i = 0; i < n; i++) {
-            int j;
-            for (j = x; j < k; j++) {
-                if (multi[i] == num[j]) {
-                    if (idx < j) {
-                        idx = j;
-                        res = i;
+            
+            changeCount++;
+            
+            int val = -1;
+            int max = -1;
+            for (int p: con) {
+                int dis = K;
+                for (int x = i; x < K; x++) {
+                    if (p == arr[x]) {
+                        dis = x - i;
+                        break;
                     }
-                    break;
+                }
+                if (max < dis) {
+                    val = p;
+                    max = dis;
                 }
             }
-            if (j == k) {
-                res = i;
-                idx = j + 1;
-            }
+            
+            con.remove(val);
+            con.add(now);
         }
-        return res;
-    }
-
+        
+		System.out.print(changeCount);
+	}
 }
